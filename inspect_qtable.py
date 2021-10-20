35eee0dffb7592cfbd4f21691f6f3d34c5e4a9c7
Varun Venkatesh <97v.varun@gmail.com>
2021-10-19 23:43:24 -0700

    Update inspect utility

--- inspect_qtable.py
+++ inspect_qtable.py
@@ -1,16 +1,26 @@
-import json
 import pickle
+import constants
 
 from game import Game
 
-state = 2147483648
+def examine_state():
+    state = 2147483648
 
-g = Game(N=5, game_state=state)
+    g = Game(N=5, game_state=state)
 
-print(g)
+    print(f'player: {state >> 50}')
+    print(g)
 
-print(f'player: {state >> 50}')
+def get_q_values_for_state(state):
+    with open(constants.Q_TABLE_FILENAME, 'rb') as fp:
+        q = pickle.load(fp)
 
+    if state in q:
+        print(q[state])
+    else:
+        print('not found')
+
+get_q_values_for_state(0)
 
 # with open('qtable.json', 'r') as fp:
 #     q = json.load(fp)
